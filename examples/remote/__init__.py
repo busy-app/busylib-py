2708da08240e3cc2de0c5d9ed21c1c22357a60bc
Sergey Trofimov <truetug@gmail.com>
2026-01-29 19:28:27 +0300

    Remove package from examples

--- examples/remote/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Remote screen streaming example.
-"""
-
-from .main import main
-from .terminal_utils import _clear_terminal, _setup_logging
-
-__all__ = ["_clear_terminal", "_setup_logging", "main"]
